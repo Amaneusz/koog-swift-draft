faba5b33da9de3a8b92d3962dd6e0027ce93ce6d
Jakub Amanowicz <jakub.amanowicz@jetbrains.com>
2026-02-06 11:26:56 +0100

    updated url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KoogAgents",
-            url: "https://github.com/JetBrains/koog/actions/runs/21741249854/artifacts/5402241516",
+            url: "https://github.com/Amaneusz/koog-swift-draft/releases/download/0.6.0/koog-agents-xcframework-01189f6ce654289a86cd2c636b7245394bac9302.zip",
             checksum: "3f13ffd0cadc58760800e1239b66a4f3823810ddcc1c143c88bbf0a100645c22"
         )
     ]
